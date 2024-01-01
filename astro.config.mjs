6f09bc4ed8102c0399ca3d47c1fa186df483acbb
Johan <johan@jeksn.me>
2024-01-01 12:15:38 +0100

    Add Vercel Analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,15 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import alpinejs from "@astrojs/alpinejs";
 import mdx from '@astrojs/mdx';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), alpinejs(), mdx()]
+  integrations: [tailwind(), alpinejs(), mdx()],
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
 });
